72ef1157849e63596b22c52571b163f5c98ebcf2
Herman Obst Demaestri <hodemaestri@gmail.com>
2024-01-29 21:35:36 -0300

    add index discovery function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::char;
 use std::fs::File;
 use std::io::Read;
 fn main() {
@@ -26,8 +27,21 @@ fn print_console_display(word: String, reconstructed_word: String) -> () {
     todo!()
 }
 
-fn main_loop(word_vec: String, max_attempts: u8) -> () {
+fn letter_index(word_string: String, letter: String) -> Vec<usize> {
+    let mut indices: Vec<usize> = Vec::new();
+    let char = letter.chars().next().unwrap();
+
+    for (index, ch) in word_string.char_indices() {
+        if ch == char {
+            indices.push(index);
+        }
+    }
+    indices
+}
+
+fn main_loop(word_string: String, max_attempts: u8) -> () {
     let mut attempts: u8 = 0;
+    let mut word: Vec<u8> = vec![0; word_string.len()];
     
     while attempts < max_attempts {
         let mut letter = String::new();
@@ -36,7 +50,7 @@ fn main_loop(word_vec: String, max_attempts: u8) -> () {
             .expect("Error leyendo la linea."); 
         letter.pop();
         
-        if word_vec.contains(&letter) {
+        if word_string.contains(&letter) {
             println!("DALEE");
         } else {
             attempts+= 1;
@@ -57,4 +71,16 @@ fn test_process_file() {
     let words = process_file(file_string);
     let expected_output = vec!["Hola", "Como", "estas?"];
     assert_eq!(words, expected_output);
+}
+
+#[test]
+fn test_letter_index() {
+    let word = "Hello".to_string();
+    let char1 = "e".to_string();
+    let char2 = "l".to_string();
+    let char0 = "x".to_string();
+
+    assert_eq!([1].to_vec(), letter_index(word.clone(), char1));
+    assert_eq!([2,3].to_vec(), letter_index(word.clone(), char2));
+    assert_eq!(Vec::<usize>::new(), letter_index(word, char0));
 }
